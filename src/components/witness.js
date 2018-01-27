1949f78023c2f9e8b8b6017790b96331ce1e5ed9
Angela <idu.angela@gmail.com>
2018-01-27 21:11:37 +0800

    add sale and witness page

--- src/components/witness.js
+++ src/components/witness.js
@@ -1,5 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { RING } from '../store/constants'
+import Ring from './ring'
 
-export default function(props) {
-  return <div>this is witness</div>
+class Witness extends Component {
+ constructor(props) {
+   super(props)
+   this.state = {
+     ring: RING,
+   };
+ }
+
+ componentWillMount() {
+   // TODO: load ring object here
+ }
+
+ render() {
+   return (
+     <div>
+       <section>
+         <h2>your address</h2>
+         <div className="avatar-row">
+           <div className="avatar">
+             <img alt="avatar" src="https://s3.amazonaws.com/cdn-live.sketch.cloud/default_avatars/m/2.png" />
+           </div>
+           <pre className="address highlight">0x00435452354265345432</pre>
+         </div>
+       </section>
+       <section>
+         <h2>my ring</h2>
+         <Ring ring={this.state.ring} useButton={false} />
+       </section>
+     </div>
+   )
+ }
 }
+
+export default Witness
