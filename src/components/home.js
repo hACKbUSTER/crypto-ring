2796b805a1c43b9b15ffab258bef70574b9d5d12
Sergio Chan <yuheng9211@qq.com>
2018-01-28 09:31:59 +0800

    。。。

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,21 @@ import img1 from '../img/pic1.png'
 import img2 from '../img/pic2.png'
 import img3 from '../img/pic3.png'
 import img4 from '../img/pic4.png'
+import contractJson from '../../build/contracts/KittyMinting.json';
+import web3js from '../web3'
 
 export default function(props) {
+
+	var MyContract = web3js.eth.contract(contractJson.abi);
+	// initiate contract for an address
+	var myContractInstance = MyContract.at('0x75c35c980c0d37ef46df04d31a140b65503c0eed');
+	
+	myContractInstance.createPromoKitty(1,web3js.eth.defaultAccount,{
+      gas: 220000,
+    }, () => console.log(arguments));
+	// myContractInstance.methods.createPromoKitty(27363,web3js.eth.defaultAccount).send().then(function(receipt){
+    // receipt can also be a new contract instance, when coming from a "contract.deploy({...}).send()"
+	// });
   return (
     <div>
       <img src={imgHome} className="home-pic" role="presentation">
