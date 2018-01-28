0bac9911485c5e2d651a9c4be9d93aaa86a7765f
Angela <idu.angela@gmail.com>
2018-01-28 09:40:53 +0800

    setInterval hack

--- src/index.js
+++ src/index.js
@@ -3,23 +3,28 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import { configureStore } from './store'
-import getWeb3 from './utils/getWeb3';
+// import getWeb3 from './utils/getWeb3';
+import web3js from './web3'
 
 let data = {}
-let web3js
+// let web3js
+//
+// getWeb3().then(results => {
+//   web3js = results.web3
+//   data.defaultAccount = web3js.eth.accounts[0];
+//   data.currentProvider = web3js.currentProvider;
+//   data.contractAddress = {
+//     minting: 0x75c35c980c0d37ef46df04d31a140b65503c0eed,
+//   };
 
-getWeb3().then(results => {
-  web3js = results.web3
-  data.defaultAccount = web3js.eth.accounts[0];
-  data.currentProvider = web3js.currentProvider;
-  data.contractAddress = {
-    minting: 0x75c35c980c0d37ef46df04d31a140b65503c0eed,
-  };
-
-  ReactDOM.render(
-    <Provider store={configureStore(data)}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  );
-})
+const int = setInterval(() => {
+  if (web3js.eth.accounts[0]) {
+    clearInterval(int);
+    ReactDOM.render(
+      <Provider store={configureStore({ defaultAccount: web3js.eth.accounts[0] })}>
+        <App />
+      </Provider>,
+      document.getElementById('root')
+    );
+  }
+}, 500);
